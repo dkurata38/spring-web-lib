b022edb5f358f649b10d18e49ac5213ecf7ac226
dkurata38 <d.kurata.38@gmail.com>
2020-03-05 16:18:21 +0900

    Gradleプロジェクトを作成

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("idea")
+    id("org.springframework.boot") version "2.2.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
+}
+
+group = "com.github.dkurata38"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
